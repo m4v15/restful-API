92a2044b76cc25bae554b6e95459220dc68a1908
Matt Davis <matthewpeterdavis@gmail.com>
2017-08-08 22:47:39 +0100

    connect to db

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 
+const mongoose = require('mongoose')
+
 const app = express()
 
 const port = process.env.PORT || 4444
@@ -7,15 +9,30 @@ const port = process.env.PORT || 4444
 const router = express.Router()
 
 // Dummy route for testing
-
 router.get('/', (req, res) => {
   res.json({ msg: 'NO BEER!' })
 })
 
-//register routes on app at /api
-
+// register routes on app at /api
 app.use('/api', router)
 
-app.listen(port)
+// Configue and connect to db
+mongoose.Promise = global.Promise
+const dbURI = 'mongodb://localhost:27017/beerlocker'
+mongoose.connect(dbURI, { useMongoClient: true })
+mongoose.connection.on('error', err => {
+  console.log('connection error: ' + err)
+})
 
-console.log('Server listening on port ', port)
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose default connection disconnected.')
+})
+
+mongoose.connection.once('open', () => {
+  app.listen(port, err => {
+    if (err) {
+      throw err
+    }
+    console.log(`Server listening on port ${port}`)
+  })
+})
