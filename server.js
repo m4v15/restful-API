71c2dc59ce9e2b03caad021c70e72983272a5e0c
Matt Davis <matthewpeterdavis@gmail.com>
2017-08-09 09:24:42 +0100

    tidy up routing

--- server.js
+++ server.js
@@ -1,75 +1,25 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-
-// Models
-const Beer = require('./models/beer')
+const beerControllers = require('./controllers/beer')
 
 // Configure App
 const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json({extended: true}))
-
-
 const port = process.env.PORT || 4444
 
+// Router
 const router = express.Router()
-
-// Dummy route for testing
-router.get('/', (req, res) => {
-  res.json({ msg: 'NO BEER!' })
-})
-
-// POST beer route
-const beersRoute = router.route('/beers')
-
-beersRoute.post((req, res) => {
-  const beer = new Beer()
-  beer.name = req.body.name
-  beer.type = req.body.type
-  beer.quantity = req.body.quantity
-
-  beer.save()
-  .then(savedBeer => {
-    res.json({ msg: 'Beer added', data: savedBeer })
-  })
-  .catch(err => res.send(err))
-})
-
-// GET all beer
-beersRoute.get((req, res) => {
-  Beer.find({})
-  .then(beers => res.json(beers))
-  .catch(err => res.send(err))
-})
-
-// GET one beer
-const beerRoute = router.route('/beers/:beer_id')
-beerRoute.get((req, res) => {
-  Beer.findById(req.params.beer_id)
-  .then(beer => res.json(beer))
-  .catch(err => res.send(err))
-})
-
-// PUT one beer
-beerRoute.put((req, res) => {
-  Beer.findById(req.params.beer_id)
-  .then(beer => {
-    beer.quantity = req.body.quantity
-    beer.save()
-    .then(beer => res.json(beer))
-    .catch(err => res.send(err))
-  })
-  .catch(err => res.send(err))
-})
-
-// DELETE one beer
-beerRoute.delete((req, res) => {
-  Beer.findByIdAndRemove(req.params.beer_id)
-  .then(() => res.json({ msg: 'Beer removed' }))
-  .catch(err => res.send(err))
-})
-
+// All beers route
+router.route('/beers')
+      .post(beerControllers.add)
+      .get(beerControllers.getAll)
+// Single beers
+router.route('/beers/:beer_id')
+      .get(beerControllers.getOne)
+      .put(beerControllers.update)
+      .delete(beerControllers.remove)
 // register routes on app at /api
 app.use('/api', router)
 
